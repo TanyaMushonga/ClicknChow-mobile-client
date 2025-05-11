8b4f79d09156fa066e8e4259d27d42a66d6d4385
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-05-11 20:37:09 +0200

    Refactor LocationHeader component to improve layout and add navigation functionality

--- components/LocationHeader.tsx
+++ components/LocationHeader.tsx
@@ -1,12 +1,57 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  useColorScheme,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useRef } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { images } from "@/constants";
+import { useRouter } from "expo-router";
 
 export default function LocationHeader() {
+  const colorScheme = useColorScheme();
+  const router = useRouter();
+
   return (
-    <View className="">
-      <Text className="text-text-primary">
-        Hello
-      </Text>
-    </View>
+    <>
+      <View className="flex flex-row w-full justify-between items-center px-2 py-4 ">
+        <TouchableOpacity onPress={() => router.push("/address")}>
+          <View className="flex flex-row items-center gap-1">
+            <MaterialIcons
+              name="location-pin"
+              size={18}
+              color={colorScheme === "dark" ? "#fff" : "#000"}
+            />
+            <Text className="text-black dark:text-white font-thin text-xl">
+              123, Main St, Nairobi
+            </Text>
+            <Entypo
+              name="chevron-small-down"
+              size={30}
+              color={colorScheme === "dark" ? "#fff" : "#000"}
+            />
+          </View>
+        </TouchableOpacity>
+        <View className="flex flex-row items-center gap-2">
+          <TouchableOpacity onPress={() => router.push("/notifications")}>
+            <MaterialCommunityIcons
+              name="bell-outline"
+              size={30}
+              color={colorScheme === "dark" ? "#fff" : "#000"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => router.push("/profile")}>
+            <Image
+              source={images.placeholder}
+              className="w-9 h-9 rounded-full border-2"
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
   );
 }
