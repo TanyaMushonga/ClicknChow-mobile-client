5bf7fa107e2fe7fffec44d18ec0d0674d0858a6f
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-05-12 17:06:37 +0200

    Refactor Home component layout: restructure ScrollView and reintroduce Search component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,25 @@
 import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import Banner from "@/components/banner";
+import Filters from "@/components/filters";
+import LastOrder from "@/components/last-order";
+import TrendingNearYou from "@/components/treding-near-you";
+import Search from "@/components/search";
+import TopMerchants from "@/components/top-merchants";
+import DealsAndCombos from "@/components/deals&combos";
 
 const Home = () => {
   return (
-    <ScrollView className="p-3 flex-1 bg-background dark:bg-background-dark mt-4">
-      <Banner />
+    <ScrollView>
+      <View className="px-3 flex-1 bg-background dark:bg-background-dark pb-5">
+        <Search />
+        <Banner />
+        <Filters />
+        <LastOrder />
+        <TrendingNearYou />
+        <TopMerchants />
+        <DealsAndCombos />
+      </View>
     </ScrollView>
   );
 };
