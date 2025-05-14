6d9d3ed506570f059e013c24104f9d707a1165b3
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-05-14 23:55:54 +0200

    Add additional food images to constants for enhanced onboarding experience

--- constants/index.ts
+++ constants/index.ts
@@ -3,10 +3,21 @@ import food2 from "../assets/images/onboarding/food2.jpg";
 import food3 from "../assets/images/onboarding/food3.jpg";
 import placeholder from "../assets/placeholder.png";
 import burger from "../assets/burger.jpg";
+import burger1 from "../assets/burger1.jpg";
+import burger2 from "../assets/burger2.jpg";
 import pasta from "../assets/pasta.jpg";
+import pasta1 from "../assets/pasta1.jpg";
+import pasta2 from "../assets/pasta2.jpg";
 import chicken from "../assets/chicken.jpg";
 import pizza from "../assets/pizza.jpg";
+import pizza1 from "../assets/pizza1.jpg";
+import pizza2 from "../assets/pizza2.jpg";
 import sushi from "../assets/sushi.jpg";
+import sushi1 from "../assets/sushi1.jpg";
+import sushi2 from "../assets/sushi2.jpg";
+import chicken1 from "../assets/chicken1.jpg";
+import chicken2 from "../assets/chicken2.jpg";
+import chicken3 from "../assets/chicken3.jpg";
 
 export const images = {
   food1,
@@ -18,6 +29,17 @@ export const images = {
   chicken,
   pizza,
   sushi,
+  chicken1,
+  chicken2,
+  chicken3,
+  pasta1,
+  pasta2,
+  sushi1,
+  sushi2,
+  pizza1,
+  pizza2,
+  burger1,
+  burger2,
 };
 
 export const onboarding = [
