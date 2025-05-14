1253c40bffd14d6b70458a40f7332db321617c13
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-05-14 23:54:38 +0200

    Add Storemap component to display a basic store map view

---
+++ app/(root)/(stack)/storemap.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Storemap = () => {
+  return (
+    <View>
+      <Text>Storemap</Text>
+    </View>
+  );
+};
+
+export default Storemap;
