73e475d3d3d418ec526101bb726f4ad6daa9ced0
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-07-17 23:48:07 +0200

    feat: Implement authentication restoration logic in Layout component

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,8 +1,39 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { Stack } from "expo-router";
+import {
+  getAccessToken,
+  getRefreshToken,
+  getUserData,
+} from "@/utils/token_storage";
+import { useIsAuthenticated } from "@/store/auth";
 
 export default function Layout() {
+  const { setIsAuthenticated, setUserData, setShowAuthModal, login } =
+    useIsAuthenticated();
+
+  useEffect(() => {
+    const restoreAuth = async () => {
+      try {
+        const access = await getAccessToken();
+        const refresh = await getRefreshToken();
+        const userData = await getUserData();
+
+        if (access && refresh && userData) {
+          login(userData);
+        } else {
+          setIsAuthenticated(false);
+          setShowAuthModal(true);
+          setUserData(null);
+        }
+      } catch (error) {
+        setIsAuthenticated(false);
+        setShowAuthModal(true);
+        setUserData(null);
+        console.error("Failed to restore authentication:", error);
+      }
+    };
+    restoreAuth();
+  }, []);
   return (
     <Stack>
       <Stack.Screen name="(stack)" options={{ headerShown: false }} />
